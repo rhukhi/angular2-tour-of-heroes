dfb2c798ab8f7f5ec49a418a381d0e118439d07b
rhukhi <rukkyujara@gmail.com>
2016-05-10 10:14:13 +0100

    Files edited, deleted, and added.

--- app/hero.js
+++ app/hero.js
@@ -1,6 +1,7 @@
 "use strict";
-var Hero = (function() {
-    function Hero() {}
+var Hero = (function () {
+    function Hero() {
+    }
     return Hero;
 }());
 exports.Hero = Hero;
--- app/mock-heroes.js
+++ app/mock-heroes.js
@@ -1,33 +1,14 @@
 "use strict";
-exports.HEROES = [{
-    "id": 11,
-    "name": "Adedeji Tomilola"
-}, {
-    "id": 12,
-    "name": "Adeka Enuwa"
-}, {
-    "id": 13,
-    "name": "Adeniji Ifeoluwatomi"
-}, {
-    "id": 14,
-    "name": "Arukwe Chiamaka"
-}, {
-    "id": 15,
-    "name": "Atawodi Ojonoka"
-}, {
-    "id": 16,
-    "name": "Eguwe Ewomazino"
-}, {
-    "id": 17,
-    "name": "Ifietekhai Rachael"
-}, {
-    "id": 18,
-    "name": "Ohikhuare Krystabel"
-}, {
-    "id": 19,
-    "name": "Richmond Blessing"
-}, {
-    "id": 20,
-    "name": "Ajisafe Mosunmoluwa"
-}];
+exports.HEROES = [
+    { "id": 11, "name": "Adedeji Tomilola" },
+    { "id": 12, "name": "Adeka Enuwa" },
+    { "id": 13, "name": "Adeniji Ifeoluwatomi" },
+    { "id": 14, "name": "Arukwe Chiamaka" },
+    { "id": 15, "name": "Atawodi Ojonoka" },
+    { "id": 16, "name": "Eguwe Ewomazino" },
+    { "id": 17, "name": "Ifietekhai Rachael" },
+    { "id": 18, "name": "Ohikhuare Krystabel" },
+    { "id": 19, "name": "Richmond Blessing" },
+    { "id": 20, "name": "Ajisafe Mosunmoluwa" }
+];
 //# sourceMappingURL=mock-heroes.js.map
